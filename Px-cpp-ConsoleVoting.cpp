48a41a3f06d6ad45e99862bbabd470f18168bb77
Percieve <111714672+Percieve@users.noreply.github.com>
2023-10-07 18:01:55 +0800

    added a file
    +1 |C++ Activity

---
+++ Px-cpp-ConsoleVoting.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+int main()
+{
+    char vote, operation;
+    int totalVotes;
+    int alphaVotes = 0;
+    int betaVotes = 0;
+    int cookieVotes = 0;
+    bool quitVoting = false;
+    while (quitVoting == false)
+    {
+        std::cout << "   PRESIDENTIAL ELECTIONS   " << std::endl;
+        std::cout << "         Candidates:        " << std::endl;
+        std::cout << "        A. ALPHA            " << std::endl;
+        std::cout << "        B. BETA             " << std::endl;
+        std::cout << "        C. COOKIE           " << std::endl;
+        std::cout << "\n";
+        std::cout << "Enter 'V'- vote, 'R'- show result and 'Q'- quit: ";
+        std::cin >> operation;
+        switch (operation)
+        {
+            case 'V':
+                std::cout << "Enter your vote: ";
+                std::cin >> vote;
+                if (vote == 'A' || vote == 'a')
+                {
+                    alphaVotes++;
+                }
+                else if (vote == 'B' || vote == 'b')
+                {
+                    betaVotes++;
+                }
+                else if (vote == 'C' || vote == 'c')
+                {
+                    cookieVotes++;
+                }
+                totalVotes = alphaVotes + betaVotes + cookieVotes;
+                break;
+            case 'R':
+                std::cout << "   PRESIDENTIAL ELECTIONS     " << std::endl;
+                std::cout << "\n";
+                std::cout << "Candidates:           Results:  " << std::endl;
+                std::cout << "A. Alpha           " << alphaVotes << std::endl;
+                std::cout << "B. Beta             " << betaVotes << std::endl;
+                std::cout << "C. Cookie            " << cookieVotes << std::endl;
+                std::cout << "   Total Votes: " << totalVotes << std::endl;
+
+                if (alphaVotes > betaVotes && alphaVotes > cookieVotes)
+                {
+                    std::cout << "   The winner is: ALPHA" << std::endl;
+                }
+                else if (betaVotes > alphaVotes && betaVotes > cookieVotes)
+                {
+                    std::cout << "   The winner is: BETA" << std::endl;
+                }
+                else if (cookieVotes > alphaVotes && cookieVotes > betaVotes)
+                {
+                    std::cout << "   The winner is: COOKIE" << std::endl;
+                }
+                break;
+            case 'Q':
+                quitVoting = true;
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
